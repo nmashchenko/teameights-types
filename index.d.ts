6060e118507eaba8e2fb65dcc2cbc48fe0e752c5
Nikita Mashchenko <mashchenko1@icloud.com>
2023-10-16 04:37:59 -0500

    feat: added auth related interfaces

--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,33 @@
 
 /** User related interfaces **/
 // Base user returned
+
+export interface IResetPassword {
+  password: string;
+  hash: string;
+}
+
+export interface IRegisterLogin {
+  email: string;
+  password: string;
+}
+
+export interface IGithubLogin {
+  code: string;
+}
+
+export interface IGoogleLogin {
+  idToken: string;
+}
+
+export interface IForgotPassword {
+  email: string;
+}
+
+export interface IConfirmEmail {
+  hash: string;
+}
+
 export interface IUserResponse {
   id: number;
   username?: string | null;
