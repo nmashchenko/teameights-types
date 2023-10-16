1399f4b5df67e4b8da396c62636eaa29e871fe61
Sivritkin Dmitriy <dmitriysivritkin@gmail.com>
2023-10-16 22:48:53 +0300

    docs: :memo: add example docs

--- src/user.ts
+++ src/user.ts
@@ -1,6 +1,22 @@
 import { NotificationType } from './notification';
 import { ITeam } from './team';
 
+/**
+ * Represents the structure of a User's data response.
+ * @property {number} id - Unique ID of the user.
+ * @property {string|null} [username] - User's chosen username.
+ * @property {string|null} [fullName] - User's full name.
+ * @property {IFileEntity|null} [photo] - User's profile photo.
+ * @property {IRole|null} [role] - User's assigned role.
+ * @property {IStatus} [status] - User's account status.
+ * @property {boolean|null} [isLeader] - Indicates if the user is a team leader.
+ * @property {string|null} [country] - Country of residence for the user.
+ * @property {Date|null} [dateOfBirth] - User's date of birth.
+ * @property {string|null} [concentration] - User's area of concentration or expertise.
+ * @property {string|null} [description] - Short bio or description about the user.
+ * @property {ExperienceType|null} [experience] - User's professional experience range.
+ * ... other properties ...
+ */
 export interface IUserResponse {
   id: number;
   username?: string | null;
@@ -28,12 +44,36 @@ export interface IUserResponse {
 }
 
 // Will be returned when admin endpoint is called or user calls /me
+/**
+ * Represents a protected view of the User's data response.
+ * This is typically used when sensitive information is required.
+ */
 export interface IUserProtectedResponse extends IUserResponse {
   email?: string | null;
   provider: string;
   socialId?: string | null;
 }
 
+/**
+ * Represents the data structure for user request details.
+ *
+ * @property {IFileEntity} [photo] - Optional reference to the user's profile photo.
+ * @property {string} [fullName] - Optional full name of the user.
+ * @property {string|null} [username] - Optional username. Can be `null` if not provided.
+ * @property {string} password - User's password.
+ * @property {boolean} [isLeader] - Optional flag indicating if the user is a leader or not.
+ * @property {string} [country] - Optional user's country of residence.
+ * @property {Date} [dateOfBirth] - Optional date of birth of the user.
+ * @property {string} [concentration] - Optional area of expertise or focus for the user.
+ * @property {string} [description] - Optional short description or bio about the user.
+ * @property {ExperienceType} experience - Professional experience range of the user.
+ * @property {string[]} [programmingLanguages] - Optional list of programming languages known by the user.
+ * @property {string[]} [frameworks] - Optional list of frameworks the user is proficient in.
+ * @property {IUniversities[]} [universities] - Optional list of universities attended by the user.
+ * @property {IJobs[]} [jobs] - Optional list of jobs held by the user.
+ * @property {IProjects[]} [projects] - Optional list of projects undertaken by the user.
+ * @property {ILinks} [links] - Optional list of links associated with the user e.g., social media or portfolios.
+ */
 export interface IUserRequest {
   photo?: IFileEntity;
   fullName?: string;
