29d98cf0bd2c7a7b77378efad67dc4f6b52f5300
Nikita Mashchenko <mashchenko1@icloud.com>
2023-10-21 05:12:02 -0500

    fix: notification types

--- src/notification.ts
+++ src/notification.ts
@@ -1,36 +1,32 @@
 /** Notifications related interfaces **/
-import { IUserResponse } from './user';
+import { IFileEntity, IUserResponse } from './user';
 import { ITeam } from './team';
+import { Identifiable, Timestamps } from './common';
 
-export interface INotificationBase {
-  id?: number;
+export interface INotificationBase extends Identifiable, Timestamps {
   user: IUserResponse;
   type: 'system' | 'team_invite';
   read: boolean;
   expiresAt: Date;
-  createdAt: Date;
-  updatedAt: Date;
-  deletedAt: Date;
 }
 
 // SystemNotification interface will have Notification fields also
 export interface ISystemNotification extends INotificationBase {
+  type: 'system'; // Discriminant property
   system_message: string;
 }
 
 // TeamInvitation interface will have Notification fields also
 export interface ITeamInvitationNotification extends INotificationBase {
-  teamid: ITeam;
-  from_user_id: IUserResponse;
+  type: 'team_invite'; // Discriminant property
+  team: ITeam;
+  from_user: IUserResponse;
   to_user_email: string;
   status: StatusType;
-  image: string;
+  photo: IFileEntity;
   message: string;
 }
 
-export type NotificationType =
-  | INotificationBase
-  | ISystemNotification
-  | ITeamInvitationNotification;
+export type NotificationType = ISystemNotification | ITeamInvitationNotification;
 
 export type StatusType = 'pending' | 'accepted' | 'rejected';
